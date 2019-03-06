fa56f8579928582f34787b82ce72b449b0a556ba
John Zlotek <jzlotek@gmail.com>
2019-03-05 21:35:59 -0500

    Basic packages and grub

--- arch.sh
+++ arch.sh
@@ -11,3 +11,28 @@ pacstrap /mnt base base-devel
 genfstab -U /mnt >> /mnt/etc/fstab
 
 arch-chroot /mnt
+
+ln -sf /usr/share/zoneinfo/EST5EDT /etc/localtime
+
+hwclock --systohc
+
+echo 'en_US.UTF-8 UTF-8' >> /etc/locale.gen
+echo 'LANG=en_US.UTF-8' >> /etc/locale.gen
+
+locale-gen
+
+echo 'arch' >> /etc/hostname
+
+echo '127.0.0.1	localhost' >> /etc/hosts
+echo '::1		localhost' >> /etc/hosts
+echo '127.0.1.1	arch.localdomain	arch' >> /etc/hosts
+
+passwd
+
+# check for intel/amd
+# pacman -Sy amd-ucode
+pacman -Sy intel-ucode
+
+pacman -Sy grub
+
+grub-mkconfig -o /boot/grub/grub.cfg
