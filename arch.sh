dd0d0daa91f0efe6225e617fca36c049777393ee
John Zlotek <jzlotek@gmail.com>
2019-03-06 18:50:58 -0500

    Fix

--- arch.sh
+++ arch.sh
@@ -1,17 +1,45 @@
 #!/bin/bash
 dialog --title "Welcome" --msgbox "This script is an automated Arch Linux bootstrapping script\n\nAn internet connection is needed to install Arch Linux" 10 40
-dialog --title "Confirmation"  --yes-label "Let's GO!" --no-label "Wait... Stop" --yesno "Please make sure that your paritions are mounted on the live disk to '/mnt'\n\n -Ready to start?" 10 40
+dialog --title "Confirmation"  --yes-label "Let's GO!" --no-label "Wait... Stop" --yesno "Please make sure that your paritions are mounted on the live disk to '/mnt'\n\n - Ready to start?" 10 40
 
 if (( $? == 1 )); then
     dialog --title "" --msgbox "Stopped bootstrap" 10 40
-    exit 1;
+    clear
+    exit 1
 fi
+
+hostname() {
+    hostname=$(dialog --title "Hostname" --inputbox "Please create your system's hostname" 10 40 "arch" 3>&1 1>&2 2>&3 3>&1)
+
+    while [[ $hostname == "" ]]; do
+        hostname=$(dialog --title "Hostname" --inputbox "Hostname cannot be blank" 10 40 "arch" 3>&1 1>&2 2>&3 3>&1)
+    done
+
+    echo "$hostname" >> /etc/hostname
+
+    echo '127.0.0.1	localhost' >> /etc/hosts
+    echo '::1		localhost' >> /etc/hosts
+    echo '127.0.1.1	'"$hostname"'.localdomain	'"$hostname" >> /etc/hosts
+}
 
 install_pacman() {
 	dialog --title "Installing pacman Packages" --infobox "Installing \`$1\` ($n of $total). $1 $2" 5 70
 	pacman --noconfirm --needed -S "$1" >/dev/null 2>&1
 }
 
+install_all_packages() {
+    packages=$(dialog --title "Select packages" --checklist "Select packages that you wish to install" 40 80 40 --file pacman.packages 3>&1 1>&2 2>&3 3>%1)
+
+    #packages=$(echo $packages | awk -F " " '{for (i=1; i<=NF; i++) print $i}')
+
+    total=$(echo $packages | awk -F " " '{for (i=1; i<=NF; i++) print $i}' | wc -l)
+
+    n=1
+    echo $packages | awk -F " " '{for (i=1; i<=NF; i++) print $i}' | while read line; do
+        install_pacman $line
+        n=$(($n+1))
+    done
+}
 run_in_chroot() {
     arch-chroot /mnt
 
@@ -37,14 +65,15 @@ run_in_chroot() {
 
     locale-gen
 
-    echo 'arch' >> /etc/hostname
-
-    echo '127.0.0.1	localhost' >> /etc/hosts
-    echo '::1		localhost' >> /etc/hosts
-    echo '127.0.1.1	arch.localdomain	arch' >> /etc/hosts
+    hostname || echo 'Error creating hostname'
 
     passwd
 
+    #refresh keyring
+    pacman -S archlinux-keyring
+    pacman -Syy
+
+    install_all_packages
 
     # check for intel/amd
     proc_type="$(lscpu | grep 'vendor_id')"
@@ -57,14 +86,16 @@ run_in_chroot() {
 
     grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=Arch\ Linux
     grub-mkconfig -o /boot/grub/grub.cfg
+
+    install_all_packages
+
     exit
 }
 
 timedatectl set-ntp true
 
 dialog --title "Running pacstrap" --infobox "Please wait while pactrap is being run" 10 40
-pacstrap /mnt base base-devel dialog 1>&2
-
+pacstrap /mnt base base-devel dialog
 
 dialog --title "Running genfstab" --infobox "Please wait while fstab is being generated" 10 40
 genfstab -U /mnt >> /mnt/etc/fstab
