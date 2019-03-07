4dc2393b63732106a7e5a74108dd90bb51e748a2
John Zlotek <jzlotek@gmail.com>
2019-03-06 19:49:54 -0500

    Change to chroot

--- arch.sh
+++ arch.sh
@@ -17,20 +17,18 @@ hostname() {
 
     echo "$hostname" >> /etc/hostname
 
-    echo '127.0.0.1	localhost' >> /etc/hosts
-    echo '::1		localhost' >> /etc/hosts
-    echo '127.0.1.1	'"$hostname"'.localdomain	'"$hostname" >> /etc/hosts
+    echo '127.0.0.1	localhost' >> /mnt/etc/hosts
+    echo '::1		localhost' >> /mnt/etc/hosts
+    echo '127.0.1.1	'"$hostname"'.localdomain	'"$hostname" >> /mnt/etc/hosts
 }
 
 install_pacman() {
 	dialog --title "Installing pacman Packages" --infobox "Installing \`$1\` ($n of $total). $1 $2" 5 70
-	pacman --noconfirm --needed -S "$1" >/dev/null 2>&1
+	arch-chroot /mnt pacman --noconfirm --needed -S "$1" >/dev/null 2>&1
 }
 
 install_all_packages() {
     packages=$(dialog --title "Select packages" --checklist "Select packages that you wish to install" 40 80 40 --file pacman.packages 3>&1 1>&2 2>&3 3>%1)
-
-    #packages=$(echo $packages | awk -F " " '{for (i=1; i<=NF; i++) print $i}')
 
     total=$(echo $packages | awk -F " " '{for (i=1; i<=NF; i++) print $i}' | wc -l)
 
@@ -40,6 +38,7 @@ install_all_packages() {
         n=$(($n+1))
     done
 }
+
 run_in_chroot() {
     arch-chroot /mnt
 
@@ -56,22 +55,22 @@ run_in_chroot() {
 
     selected_zone=$(dialog --title "Time Zone selection" --radiolist "Select your time zone" 40 80 40 $(echo $time_zones | awk -F '\n' '{ if ($1) print $1, $1, "0" }') 3>&1 1>&2 2>&3 3>&1)
 
-    ln -sf /usr/share/zoneinfo/"$selected_zone" /etc/localtime
+    arch-chroot /mnt ln -sf /usr/share/zoneinfo/"$selected_zone" /etc/localtime
 
-    hwclock --systohc
+    arch-chroot /mnt hwclock --systohc
 
-    echo 'en_US.UTF-8 UTF-8' >> /etc/locale.gen
-    echo 'LANG=en_US.UTF-8' >> /etc/locale.gen
+    echo 'en_US.UTF-8 UTF-8' >> /mnt/etc/locale.gen
+    echo 'LANG=en_US.UTF-8' >> /mnt/etc/locale.gen
 
-    locale-gen
+    arch-chroot /mnt locale-gen
 
     hostname || echo 'Error creating hostname'
 
-    passwd
+    arch-chroot /mnt passwd
 
     #refresh keyring
-    pacman -S archlinux-keyring
-    pacman -Syy
+    arch-chroot /mnt pacman -S archlinux-keyring
+    arch-chroot /mnt pacman -Syy
 
     install_all_packages
 
@@ -79,13 +78,13 @@ run_in_chroot() {
     proc_type="$(lscpu | grep 'vendor_id')"
 
     if [[ $(proc_type) =~ /intel/i  ]]; then
-        pacman -S --noconfirm --needed intel-ucode grub
+        arch-chroot /mnt pacman -S --noconfirm --needed intel-ucode grub
     else
-        pacman -S --noconfirm --needed amd-ucode grub
+        arch-chroot /mnt pacman -S --noconfirm --needed amd-ucode grub
     fi
 
-    grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=Arch\ Linux
-    grub-mkconfig -o /boot/grub/grub.cfg
+    arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=Arch\ Linux
+    arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 
     install_all_packages
 
@@ -100,4 +99,4 @@ pacstrap /mnt base base-devel dialog
 dialog --title "Running genfstab" --infobox "Please wait while fstab is being generated" 10 40
 genfstab -U /mnt >> /mnt/etc/fstab
 
-arch-chroot /mnt run_in_chroot
+arch-chroot /mnt
